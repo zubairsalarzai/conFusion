57b11fd75d5fc6854da32bfbdb75eb2058d29e94
Muhammad Zubair <muhzubair321@gmail.com>
2020-07-09 12:36:35 +0500

    component part one

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    id:string;
+    name:string;
+    image:string;
+    category:string;
+    featured:Boolean;
+    label:string;
+    price:string;
+    description:string;
+}
